491f98a57690f2788c752712daf612b4826b7ee0
jashook <jshook@smu.edu>
2014-12-13 18:41:53 -0600

    update to the header

--- CarDistanceDetection/iOSApp/DistanceAppM/ViewController.h
+++ CarDistanceDetection/iOSApp/DistanceAppM/ViewController.h
@@ -9,14 +9,25 @@
 #import <UIKit/UIKit.h>
 #import "BLE.h"
 #import <AudioToolbox/AudioToolbox.h>
+#import <CoreImage/CoreImage.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController : UIViewController <BLEDelegate> {
     BLE* m_ble_endpoint;
 }
 
 @property (weak, nonatomic) IBOutlet UILabel *DistanceLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *PersonLabel;
+
+@property (strong, nonatomic) NSDictionary *detectorOptions;
+@property (strong, nonatomic) CIDetector *face_detector;
+@property (strong, nonatomic) NSArray *features;
+@property (strong, nonatomic) UIImage *image;
+
+@property AVCaptureSession *session;
+
+@property bool _eyes;
+@property int _count;
 
 @property bool did_sound_play;
 
